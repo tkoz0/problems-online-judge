8014538616fd7ad4a34bda9d386dc9fbef03406c
TKoz <TKoz@__WINDOWS__>
2018-01-02 06:03:43 -0500

    wrote dynamic solution, many memory errors

--- vol_001/p116.cpp
+++ vol_001/p116.cpp
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <assert.h>
 
+// for std::swap
+#include <algorithm>
+
 typedef uint32_t u32;
 typedef int32_t i32;
 
@@ -25,7 +28,56 @@ void solve_backwards_dynamic(i32 **matr, u32 rows, u32 cols)
         printf("\n%i\n", sum);
         return;
     }
-    // at least 2 rows
+    // at least 2 rows, allocate temporary space to store dynamic path sums
+    i32 *sums = (i32*) calloc(rows, sizeof(i32)); // start zero
+    i32 *tmp = (i32*) malloc(rows * sizeof(i32)); // values set each iteration
+    u32 col = cols; // start at last column, move towards start
+    while (col--)
+    {
+        i32 ri; // for row index selection (minimum lexicographic order)
+        // sums stores dynamic values for min paths starting from each of
+        // the rows in the next columns, use those to work backwards 1 column
+        // minimum row separately first (next 0, 1, row-1)
+        ri = 0;
+        if (sums[1] < sums[ri]) ri = 1;
+        if (sums[rows - 1] < sums[ri]) ri = rows - 1;
+        tmp[0] = sums[ri] + matr[0][col];
+        matr[0][col] = ri;
+        // maximum row separately next (next row-2, row-1, 0)
+        ri = 0;
+        if (sums[rows - 2] < sums[ri]) ri = rows - 2;
+        if (sums[rows - 1] < sums[ri]) ri = rows - 1;
+        tmp[rows - 1] = sums[ri] + matr[rows - 1][col];
+        matr[rows - 1][col] = ri;
+        // all other rows (1...row-2) (rows in the middle)
+        i32 **rp = matr + 1;
+        i32 *tmpp = tmp + 1; // pointer to temp array
+        i32 rn = 1; // row number
+        for (; rp != matr + rows - 1; ++rp, ++rn, ++tmp)
+        {
+            ri = rn - 1; // initial row index selection
+            if (sums[rn] < sums[ri]) ri = rn;
+            if (sums[rn + 1] < sums[ri]) ri = rn + 1;
+            *tmpp = sums[ri] + (*rp)[col]; // new min path sum, 1 step backward
+            (*rp)[col] = ri; // store next row index
+        }
+        std::swap(tmp, sums); // swap roles of tmp and sums arrays
+    }
+    // select minimum path from sums array
+    i32 r = 0; // find start row
+    for (u32 i = 1; i != rows; ++i)
+        if (sums[i] < sums[r]) r = i;
+    printf("%i", r);
+    i32 minpathval = sums[r]; // save min path value to print after path
+    // follow path to print remaining values
+    for (col = 0; col != cols - 1; ++col) // ignore last column
+    {
+        r = matr[r][col];
+        printf(" %i", r);
+    }
+    printf("\n%i\n", minpathval);
+    free(sums);
+    free(tmp);
 }
 
 int main(int argc, char **argv)
@@ -34,7 +86,7 @@ int main(int argc, char **argv)
     // allocate a matrix with the maximum required space
     // each input problem will overwrite whatever space it needs
     i32 **matr = (i32**) malloc(MAX_ROW * sizeof(i32*));
-    for (i32 **i = matr; i != matr + rows; ++i)
+    for (i32 **i = matr; i != matr + MAX_ROW; ++i)
         *i = (i32*) malloc(MAX_COL * sizeof(i32));
     // read input problems until EOF
     while (scanf("%u %u\n", &rows, &cols) == 2)
