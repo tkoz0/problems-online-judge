e5d3f9de0afec674dd25425519b7752f116ae5ac
Your Name <you@example.com>
2018-01-11 16:32:08 -0500

    debugged input code

--- vol_001/p109.cpp
+++ vol_001/p109.cpp
@@ -13,6 +13,7 @@ typedef uint32_t u32;
 struct pt
 {
     u32 x, y;
+    pt() {}
     pt(u32 x, u32 y)
     {
         assert(x <= COORD_LIMIT);
@@ -28,13 +29,28 @@ struct kingdom
     std::vector<pt> _homes;
     kingdom(u32 ppx, u32 ppy)
     {
-        this->_pp(ppx, ppy);
+        this->_pp = pt(ppx, ppy);
     }
     void add_home(u32 x, u32 y)
     {
         this->_homes.push_back(pt(x, y));
     }
 };
+
+void _debug_print(std::vector<kingdom>& k, std::vector<pt>& m)
+{
+    for (u32 i = 0; i != k.size(); ++i)
+    {
+        printf("k%02u(%u,%u):", i+1, k[i]._pp.x, k[i]._pp.y);
+        for (auto j = k[i]._homes.begin(); j != k[i]._homes.end(); ++j)
+            printf(" (%u,%u)", j->x, j->y);
+        printf("\n");
+    }
+    printf("missiles:");
+    for (auto i = m.begin(); i != m.end(); ++i)
+        printf(" (%u,%u)", i->x, i->y);
+    printf("\n");
+}
 
 int main(int argc, char **argv)
 {
@@ -61,13 +77,14 @@ int main(int argc, char **argv)
             assert(r == 2);
             k.add_home(x, y);
         }
+        kingdoms.push_back(k);
     }
     assert(kingdoms.size() <= MAX_KINGDOMS);
     assert(read_missiles); // should have reached a N=-1 kingdom before this
     u32 x, y;
     while (scanf("%u %u\n", &x, &y) == 2)
         missiles.push_back(pt(x, y));
-    ;
+//    _debug_print(kingdoms, missiles);
     return 0;
 }
 
