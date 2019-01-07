62943ca48a26f2a284cd8ead7f7289946bdba8cc
TKoz Zero <tkozlowski1999@gmail.com>
2019-01-06 22:09:53 -0500

    p10810 accepted, count inversions with 64 bit integer

--- vol_108/p10810.cpp
+++ vol_108/p10810.cpp
@@ -7,9 +7,9 @@ uint32_t arr[500000];
 uint32_t tmp[500000];
 
 // brute force O(n^2) for debugging
-uint32_t count_inversions0(uint32_t beg, uint32_t end)
+uint64_t count_inversions0(uint32_t beg, uint32_t end)
 {
-    uint32_t invs = 0;
+    uint64_t invs = 0;
     for (uint32_t i = beg; i != end; ++i)
         for (uint32_t j = i+1; j != end; ++j)
             if (arr[i] > arr[j]) ++invs;
@@ -18,12 +18,12 @@ uint32_t count_inversions0(uint32_t beg, uint32_t end)
 
 // count inversions in the range [beg,end)
 // this one uses a merge sort method
-uint32_t count_inversions1(uint32_t beg, uint32_t end)
+uint64_t count_inversions1(uint32_t beg, uint32_t end)
 {
     if (beg+1 >= end) return 0; // no inversions in empty or length 1 array
     uint32_t mid = (beg+end)>>1; // split to [beg,mid) and [mid,end)
     // recursively count inversions undone in the 2 halves
-    uint32_t invs = count_inversions1(beg,mid) + count_inversions1(mid,end);
+    uint64_t invs = count_inversions1(beg,mid) + count_inversions1(mid,end);
     // merge the 2 halves into tmp and count the inversions undone
     uint32_t L = beg, R = mid; // next index in each half
     for (uint32_t T = beg; T != end; ++T) // index in tmp
@@ -52,9 +52,9 @@ int main(int argc, char **argv)
         assert(n < 500000);
         for (p = arr; p != arr+n; ++p)
             scanf("%u",p);
-        printf("insertion %u\n",count_inversions0(0,n));
-        printf("merge %u\n",count_inversions1(0,n));
-//        printf("%u\n",count_inversions1(0,n));
+//        printf("insertion %lu\n",count_inversions0(0,n));
+//        printf("merge %lu\n",count_inversions1(0,n));
+        printf("%lu\n",count_inversions1(0,n));
     }
     return 0;
 }
