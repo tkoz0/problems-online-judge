c0b749b7161b0ae06152e2adb63b65f078513b18
TKoz Zero <tkozlowski1999@gmail.com>
2019-01-13 22:47:16 -0500

    p10190 more comments

--- vol_101/p10190.py
+++ vol_101/p10190.py
@@ -2,12 +2,13 @@ import sys
 
 for line in sys.stdin:
     n, m = map(int,line.split())
-    # cannot create division sequence
+    # cannot create division sequence (length at least 2)
     if m < 2 or n == 0 or m > n: print('Boring!'); continue
     seq = [n]
-    while n > 1:
+    while n > 1: # try to make a sequence that works
         if n % m != 0: break
-        n //= m
+        n //= m # will get smaller since m >= 2
         seq.append(n)
+    # successfully reached 1
     if seq[-1] == 1: print(' '.join(map(str,seq)))
     else: print('Boring!')
