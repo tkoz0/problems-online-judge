7e85a969dac6412c749ba41b388060e606a641f8
TKoz Zero <tkozlowski1999@gmail.com>
2019-01-17 23:48:58 -0500

    p10487 solution

--- vol_104/p10487.py
+++ vol_104/p10487.py
@@ -10,6 +10,32 @@ while True:
         nums.append(int(input()))
     m = int(input())
     assert 0 < m < 25
+
+    # find all possible sums
+    nums = set(nums)
+    sums = set()
+    for z1 in nums:
+        for z2 in nums:
+            if z1 != z2: sums.add(z1+z2)
+    sums = sorted(sums) # sort for binary searching
+
     print('Case %d:'%case)
+
     for _ in range(m): # process queries
-        pass
+        query = int(input())
+        # binary search for near number
+        lo, hi = 0, len(sums)-1
+        while lo != hi:
+            mid = (lo+hi)//2 # split into [lo,mid] and [mid+1,hi]
+            if query <= sums[mid]:
+                hi = mid
+            else: lo = mid+1
+        # will be either sums[lo] or sums[lo+1] or sums[lo-1]
+        # sums[lo] is the largest value that is <= query
+        closest = sums[lo]
+        if lo+1<len(sums) and abs(sums[lo+1]-query) < abs(query-closest):
+            closest = sums[lo+1] # has smaller absolute difference
+        if lo>0 and abs(query-sums[lo-1]) < abs(query-closest):
+            closest = sums[lo-1] # has smaller absolute difference
+        print('Closest sum to %d is %d.'%(query,closest))
+
