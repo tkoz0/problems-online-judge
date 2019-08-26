307caa7f40d751001bf0699129b211048fb73f84
TKoz Zero <tkozlowski1999@gmail.com>
2019-08-25 21:00:18 -0400

    moved big array from stack to heap

--- vol_001/p199.c
+++ vol_001/p199.c
@@ -17,7 +17,11 @@ int main(int argc, char **argv)
     u32 xof[(MAX_N-1)*(MAX_N-1)]; // max P to x
     u32 yof[(MAX_N-1)*(MAX_N-1)]; // map P to y
     i32 B[1+MAX_N][1+MAX_N]; // store boundary values
-    i32 a[(MAX_N-1)*(MAX_N-1)][(MAX_N-1)*(MAX_N-1)]; // for solution
+    i32 **a; // reallocated on heap (probably is too much for stack)
+    a = malloc(sizeof(*a)*(MAX_N-1)*(MAX_N-1));
+    for (u32 z = 0; z < (MAX_N-1)*(MAX_N-1); ++z)
+        a[z] = malloc(sizeof(**a)*(MAX_N-1)*(MAX_N-1));
+//    i32 a[(MAX_N-1)*(MAX_N-1)][(MAX_N-1)*(MAX_N-1)]; // for solution
     i32 b[(MAX_N-1)*(MAX_N-1)]; // for solution, both solutions offset by 1
     if (scanf("%u",&m) != 1) assert(0); // number of tests
     while (m--)
@@ -38,7 +42,7 @@ int main(int argc, char **argv)
         for (u32 i = 0; i <= n; ++i)
         {
             if (scanf("%i",b2+i) != 1) assert(0);
-            B[i][1] = b2[i];
+            B[i][n] = b2[i];
         }
         for (u32 i = 0; i <= n; ++i)
         {
@@ -48,15 +52,27 @@ int main(int argc, char **argv)
         for (u32 i = 0; i <= n; ++i)
         {
             if (scanf("%i",b4+i) != 1) assert(0);
-            B[1][i] = b4[i];
+            B[n][i] = b4[i];
         }
         assert(b1[0] == b3[0]);
         assert(b2[0] == b3[n]);
         assert(b2[n] == b4[n]);
         assert(b1[n] == b4[0]);
+
+        // printf("boundary\n");
+        // for (u32 y = n+1; y--;)
+        // {
+        //     for (u32 x = 0; x <= n; ++x) printf("%i ",B[x][y]);
+        //     printf("\n");
+        // }
+        // printf("end boundary\n");
+
         for (u32 y = n+1; y--;) // f
             for (u32 x = 0; x <= n; ++x)
                 if (scanf("%i",&f[x][y]) != 1) assert(0);
+
+//        continue; // debug
+
         u32 pnum = 0;
         for (u32 y = n+1; y--;) // setup P matrix
             for (u32 x = 0; x <= n; ++x)
@@ -80,11 +96,17 @@ int main(int argc, char **argv)
             for (i32 x2 = -1; x2 <= 1; ++x2) // v and g matrices
                 for (i32 y2 = -1; y2 <= 1; ++y2)
                 {
+//                    printf("p%u adding %i*%i\n",p,g[1+x2][1+y2],f[x+x2][y+y2]);
                     rhs += g[1+x2][1+y2]*f[x+x2][y+y2]; // g contributes to rhs
                     if (P[x+x2][y+y2]) // P number, v contributes to main matrix
+                    {
                         a[p-1][P[x+x2][y+y2]-1] = v[1+x2][1+y2] *n*n;
+                    }
                     else // get from boundary, contribute to rhs
+                    {
+//                        printf("p%u subtracting %i\n",p,v[1+x2][1+y2]*B[x+x2][y+y2] *n*n);
                         rhs -= v[1+x2][1+y2]*B[x+x2][y+y2] *n*n;
+                    }
                 }
             b[p-1] = rhs; // TODO wrong, fix
         }
