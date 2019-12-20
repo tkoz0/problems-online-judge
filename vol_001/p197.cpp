061085d2dd8417f4361553935da041faf71146fe
TKoz Zero <tkozlowski1999@gmail.com>
2019-12-19 22:32:22 -0500

    p197 cpp code initial

---
+++ vol_001/p197.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+// find all possibilities considering one orientation of block "a"
+// to get all the others, rotate the solutions found and identify the solutions
+// set based on the orientation of block "a"
+// output the appropriate solution set when given user input
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
