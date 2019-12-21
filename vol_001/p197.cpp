0168d0478832bab0389dff29d4d088d010d634dc
TKoz Zero <tkozlowski1999@gmail.com>
2019-12-21 07:43:40 -0500

    p197 python to c++ translation done

--- vol_001/p197.cpp
+++ vol_001/p197.cpp
@@ -17,15 +17,15 @@ typedef std::vector<char> char1d;
 // output the appropriate solution set when given user input
 
 // create cube and get dimensions
-u32 xlen(char3d& c) { return c.size(); }
-u32 ylen(char3d& c) { return c[0].size(); }
-u32 zlen(char3d& c) { return c[0][0].size(); }
+u32 xlen(const char3d& c) { return c.size(); }
+u32 ylen(const char3d& c) { return c[0].size(); }
+u32 zlen(const char3d& c) { return c[0][0].size(); }
 char3d new_cube(u32 X, u32 Y, u32 Z)
 {
     return char3d(X,char2d(Y,char1d(Z,'.')));
 }
 
-std::string encode(char3d& cube) // cube --> 27 char string
+std::string encode(const char3d& cube) // cube --> 27 char string
 {
     assert(xlen(cube) == 3 and ylen(cube) == 3 and zlen(cube) == 3);
     std::string s = "";
@@ -36,7 +36,7 @@ std::string encode(char3d& cube) // cube --> 27 char string
     return s;
 }
 
-char3d decode(std::string& s) // 27 char string --> cube
+char3d decode(const std::string& s) // 27 char string --> cube
 {
     assert(s.length() == 27);
     char3d cube = new_cube(3,3,3);
@@ -58,39 +58,39 @@ char3d G0 = {{{'.','.'},{'g','g'}},{{'.','g'},{'.','g'}}};
 std::vector<char3d> A, B, C, D, E, F, G; // for all possible orientations
 
 // rotation functions for 2d arrays
-char2d rot0(char2d& flat)
+char2d rot0(const char2d& flat)
 {
-    auto rot = char2d(flat.size(),char1d('.',flat[0].size()));
+    auto rot = char2d(flat.size(),char1d(flat[0].size()));
     for (u32 x = 0; x < flat.size(); ++x)
         for (u32 y = 0; y < flat[0].size(); ++y)
             rot[x][y] = flat[x][y];
     return rot;
 }
-char2d rot90(char2d& flat)
+char2d rot90(const char2d& flat)
 {
-    auto rot = char2d(flat[0].size(),char1d('.',flat.size()));
+    auto rot = char2d(flat[0].size(),char1d(flat.size()));
     for (u32 x = 0; x < flat[0].size(); ++x)
         for (u32 y = 0; y < flat.size(); ++y)
             rot[x][y] = flat[y][flat[0].size()-1-x];
     return rot;
 }
-char2d rot180(char2d& flat)
+char2d rot180(const char2d& flat)
 {
-    auto rot = char2d(flat.size(),char1d('.',flat[0].size()));
+    auto rot = char2d(flat.size(),char1d(flat[0].size()));
     for (u32 x = 0; x < flat.size(); ++x)
         for (u32 y = 0; y < flat[0].size(); ++y)
             rot[x][y] = flat[flat.size()-1-x][flat[0].size()-1-y];
     return rot;
 }
-char2d rot270(char2d& flat)
+char2d rot270(const char2d& flat)
 {
-    auto rot = char2d(flat[0].size(),char1d('.',flat.size()));
+    auto rot = char2d(flat[0].size(),char1d(flat.size()));
     for (u32 x = 0; x < flat[0].size(); ++x)
         for (u32 y = 0; y < flat.size(); ++y)
             rot[x][y] = flat[flat.size()-1-y][x];
     return rot;
 }
-std::vector<char3d> all_rots(char3d& cube) // rotate around x axis
+std::vector<char3d> all_rots(const char3d& cube) // rotate around x axis
 {
     std::vector<char3d> results;
     char3d cube1 = char3d(xlen(cube));
@@ -108,7 +108,7 @@ std::vector<char3d> all_rots(char3d& cube) // rotate around x axis
     return results;
 }
 
-std::vector<char3d> compute_rotations(char3d& cube) // get all 24 rotations
+std::vector<char3d> compute_rotations(const char3d& cube) // all 24 rotations
 {
     u32 X = xlen(cube);
     u32 Y = ylen(cube);
@@ -153,7 +153,7 @@ std::vector<char3d> compute_rotations(char3d& cube) // get all 24 rotations
     return results;
 }
 
-std::vector<char3d> dedup(std::vector<char3d>& l)
+std::vector<char3d> dedup(const std::vector<char3d>& l)
 {
     auto l2 = std::vector<char3d>();
     for (char3d cube : l)
@@ -168,6 +168,32 @@ std::vector<char3d> dedup(std::vector<char3d>& l)
         if (not in_l2) l2.push_back(cube);
     }
     return l2;
+}
+
+char3d truncate(const char3d& cube) // assumes cube is nonempty
+{
+    // first find coordinate bounds
+    u32 xm = -1, xM = 0, ym = -1, yM = 0, zm = -1, zM = 0, x, y, z;
+    for (x = 0; x < xlen(cube); ++x)
+        for (y = 0; y < ylen(cube); ++y)
+            for (z = 0; z < zlen(cube); ++z)
+            {
+                if (cube[x][y][z] == '.') continue; // empty
+                xm = std::min(xm,x);
+                ym = std::min(ym,y);
+                zm = std::min(zm,z);
+                xM = std::max(xM,x);
+                yM = std::max(yM,y);
+                zM = std::max(zM,z);
+            }
+    // extract subcube that contains nonempty cells
+    u32 X = 1+xM-xm, Y = 1+yM-ym, Z = 1+zM-zm;
+    char3d result = new_cube(X,Y,Z);
+    for (x = 0; x < X; ++x)
+        for (y = 0; y < Y; ++y)
+            for (z = 0; z < Z; ++z)
+                result[x][y][z] = cube[xm+x][ym+y][zm+z];
+    return result;
 }
 
 void initialize_blocks() // generate block orientation data
@@ -189,19 +215,131 @@ void initialize_blocks() // generate block orientation data
     G = dedup(rotG);
 }
 
-void _validate_cube_input(std::string& s) // input should only have "a" block
+void _validate_cube_input(const std::string& s) // input only "a" block
 {
     for (char c : s)
         assert(c == '.' or c == 'a');
 }
 
+bool _contains(const std::vector<char3d>& items, char3d& e)
+{
+    bool contains = false;
+    for (char3d block : items)
+        if (block == e)
+        {
+            contains = true;
+            break;
+        }
+    return contains;
+}
+
+void _print_piece(const char3d& p)
+{
+    printf("%u,%u,%u{",xlen(p),ylen(p),zlen(p));
+    for (u32 x = 0; x < xlen(p); ++x)
+    {
+        printf("{");
+        for (u32 y = 0; y < ylen(p); ++y)
+        {
+            printf("{%c",p[x][y][0]);
+            for (u32 z = 1; z < zlen(p); ++z)
+                printf(",%c",p[x][y][z]);
+            printf("}");
+        }
+        printf("}");
+    }
+    printf("\n");
+}
+
 std::vector<char3d> initial_solutions;
 char3d rec_cube;
 
-void find_solutions_a()
+bool place_block(u32 x0, u32 y0, u32 z0, const char3d& block)
 {
-    printf("%lu %lu %lu %lu %lu %lu %lu\n",A.size(),B.size(),C.size(),
-                                    D.size(),E.size(),F.size(),G.size());
+    u32 X = xlen(block), Y = ylen(block), Z = zlen(block), x, y, z;
+    if (x0 + X > 3 or y0 + Y > 3 or z0 + Z > 3) return false;
+    for (x = 0; x < X; ++x) // check if can place
+        for (y = 0; y < Y; ++y)
+            for (z = 0; z < Z; ++z)
+                if (block[x][y][z] != '.' and rec_cube[x0+x][y0+y][z0+z] != '.')
+                    return false; // cannot overwrite occupied space
+    for (x = 0; x < X; ++x) // place
+        for (y = 0; y < Y; ++y)
+            for (z = 0; z < Z; ++z)
+                if (block[x][y][z] != '.')
+                    rec_cube[x0+x][y0+y][z0+z] = block[x][y][z];
+//printf("place | %u %u %u | %u %u %u\n",x0,y0,z0,X,Y,Z);
+    return true;
+}
+
+void remove_block(u32 x0, u32 y0, u32 z0, const char3d& block)
+{
+    u32 X = xlen(block), Y = ylen(block), Z = zlen(block), x, y, z;
+//printf("remove | %u %u %u | %u %u %u\n",x0,y0,z0,X,Y,Z);
+    assert(x0 + X <= 3 and y0 + Y <= 3 and z0 + Z <= 3);
+    for (x = 0; x < X; ++x)
+        for (y = 0; y < Y; ++y)
+            for (z = 0; z < Z; ++z)
+                if (block[x][y][z] != '.') // remove cell from the block
+                    rec_cube[x0+x][y0+y][z0+z] = '.';
+}
+
+void find_solutions(char code) // recurse on pieces b,c,d,e,f,g
+{
+    if (code == 'h') initial_solutions.push_back(rec_cube);
+    else
+    {
+        std::vector<char3d> *possible_ = &A;
+        switch (code)
+        {
+            case 'b': possible_ = &B; break;
+            case 'c': possible_ = &C; break;
+            case 'd': possible_ = &D; break;
+            case 'e': possible_ = &E; break;
+            case 'f': possible_ = &F; break;
+            case 'g': possible_ = &G; break;
+            default: assert(0);
+        }
+        const std::vector<char3d>& possible = *possible_;
+//        for (auto block : possible)
+        for (auto itr = possible.cbegin(); itr != possible.cend(); ++itr)
+        {
+            const char3d& block = *itr;
+            u32 X = xlen(block), Y = ylen(block), Z = zlen(block);
+            for (u32 x0 = 0; x0 < 4-X; ++x0)
+                for (u32 y0 = 0; y0 < 4-Y; ++y0)
+                    for (u32 z0 = 0; z0 < 4-Z; ++z0)
+                        if (place_block(x0,y0,z0,block))
+                        {
+//printf("placed %c %u %u %u\n",code,xlen(block),ylen(block),zlen(block));
+                            find_solutions((char)(code+1));
+//printf("removing %c %u %u %u\n",code,xlen(block),ylen(block),zlen(block));
+                            remove_block(x0,y0,z0,block);
+                        }
+        }
+    }
+}
+
+void find_solutions_a() // translate the "a" piece in one of its orientations
+{
+//printf("%lu %lu %lu %lu %lu %lu %lu\n",A.size(),B.size(),C.size(),
+//                                D.size(),E.size(),F.size(),G.size());
+    char3d a_piece = truncate(rec_cube);
+//printf("%u %u %u\n",xlen(a_piece),ylen(a_piece),zlen(a_piece));
+//    for (auto p : A) _print_piece(p);
+//    _print_piece(a_piece);
+    assert(_contains(A,a_piece));
+    u32 X = xlen(a_piece), Y = ylen(a_piece), Z = zlen(a_piece);
+    for (u32 x0 = 0; x0 < 4-X; ++x0)
+        for (u32 y0 = 0; y0 < 4-Y; ++y0)
+            for (u32 z0 = 0; z0 < 4-Z; ++z0)
+            {
+//printf("find_solutions_a(): %u %u %u\n",x0,y0,z0);
+                rec_cube = new_cube(3,3,3);
+                bool success = place_block(x0,y0,z0,a_piece);
+                assert(success);
+                find_solutions('b');
+            }
 }
 
 int main(int argc, char **argv)
@@ -212,6 +350,7 @@ int main(int argc, char **argv)
     rec_cube = decode(initial);
     initial_solutions.clear();
     find_solutions_a(); // begin recursion to find solutions
+printf("%lu\n",initial_solutions.size());
     // hard coded check for number of solutions, believed to be 480
     assert(initial_solutions.size() == 480);
     // TODO compute all rotations for solutions
